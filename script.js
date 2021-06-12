67c295a3581e55216256b457b3cbaf223ea10dbd
brunodias200 <brunoadias1996@gmail.com>
2021-06-12 08:26:38 -0300

    commit2

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ for(i=1;i<=12;i++){
     divclassclock.innerHTML += `<div class="number n${i}" style="--rotation: ${i*30}deg">${i}</div>`
 }/*/criar o relogio*/
 
+setInterval(setData, 1000)
+
+const hora = document.querySelector('[ponteiro-hora]')
+const minuto = document.querySelector('[ponteiro-minuto]')
+const segundo = document.querySelector('[ponteiro-segundo]')
+
+function setData(){
+    var data = new Date()
+    var segundos = data.getSeconds()/60
+    var minutos = (segundos + data.getMinutes())/60
+    var horas = (minutos + data.getHours())/12
+    setRotacao(segundo, segundos)
+    setRotacao(minuto, minutos)
+    setRotacao(hora, horas)
+}
+
+function setRotacao(elemento, rotacao){
+    elemento.style.setProperty('--rotation', rotacao*360)
+}
+
+setData()
